6e215c200b738f51fac8e31a68c8004d9f55f4cb
Isaac Wolff <wolff215@gmail.com>
2023-02-13 08:52:19 -0600

    Made few changes

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from calc import check_activities, athlete_info
 
 app = Flask(__name__)
 
+entries = check_activities()
+athlete = athlete_info()
+
 @app.route('/')
 def index():
-    entries = check_activities()
-    athlete = athlete_info()
     return render_template('index.html', entries=entries, athlete=athlete)
 
 if __name__ == '__main__':
